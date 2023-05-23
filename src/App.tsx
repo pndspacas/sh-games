0eace59438f59711a1d75c005fb2bb0616f58613
Pedro <pndspacas@gmail.com>
2023-05-22 22:12:32 -0700

    Fix menus

--- src/App.tsx
+++ src/App.tsx
@@ -1,4 +1,4 @@
-import { Grid, GridItem, HStack, Show } from '@chakra-ui/react'
+import { Box, Flex, Grid, GridItem, Show } from '@chakra-ui/react'
 import { Navbar } from './components/Navbar'
 import { GamesGrid } from './components/GamesGrid'
 import GenreList from './components/GenreList'
@@ -36,10 +36,13 @@ function App() {
       </GridItem>
     </Show>
     <GridItem area="main">
-      <HStack paddingLeft={10}>
-        <PlatformSelector selectedPlatform={gameQuery.platform} onSelectPlatform={(platform) => setGameQuery({ ...gameQuery, platform })} />
+      <Flex paddingLeft={10}>
+        <Box marginRight={2}>
+          <PlatformSelector selectedPlatform={gameQuery.platform} onSelectPlatform={(platform) => setGameQuery({ ...gameQuery, platform })} />
+        </Box>
+
         <SortSelector sortOrder={gameQuery.sortOrder} onSelectSortOrder={(sortOrder) => setGameQuery({ ...gameQuery, sortOrder })} />
-      </HStack>
+      </Flex>
       <GamesGrid gameQuery={gameQuery} />
     </GridItem>
   </Grid>
